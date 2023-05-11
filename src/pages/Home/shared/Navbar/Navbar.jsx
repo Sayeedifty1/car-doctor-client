110369a68f02a4b0128a065bfa2748231ce20294
Sayeedifty1 <md.abusayeedifty@gmail.com>
2023-05-11 14:47:42 +0600

    logout added

--- src/pages/Home/shared/Navbar/Navbar.jsx
+++ src/pages/Home/shared/Navbar/Navbar.jsx
@@ -1,10 +1,22 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 import logo from '../../../../assets/logo.svg'
+import { AuthContext } from "../../../../providers/AuthProvider";
 
 const Navbar = () => {
+    const {user , logOut} = useContext(AuthContext);
+
+    const handleLogOut = ()=>{
+        logOut()
+        .then(()=>{})
+        .catch(error => console.log(error))
+    }
     const navItems = <>
         <li><Link to="/">Home</Link> </li>
         <li> <Link to="/about">About</Link> </li>
+        {user?.email? <li><button onClick={handleLogOut}>Log out</button></li>: 
+        <li> <Link to="/login">LogIn</Link> </li>
+        }
     </>
     return (
         <div className="navbar bg-base-100 h-28 mb-4">
